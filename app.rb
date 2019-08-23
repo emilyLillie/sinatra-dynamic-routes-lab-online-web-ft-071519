482ec08780736e405d5d0f7534161388449f3961
Emily Lillie <emilylillie3@gmail.com>
2019-08-23 23:18:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,10 +11,8 @@ class App < Sinatra::Base
     @num = params[:number]
     "#{@num.to_i ** 2}"
   end
-  # get '/square/:number' do 
-  #   @num = params[:number]
-  #   "#{@num.to_i ** 2}"
-  # end
+ 
+ 
 
 
 end
