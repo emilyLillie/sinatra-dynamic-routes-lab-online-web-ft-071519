6d7f7a44eca12e3939aba34c0079aeab04794e1a
Emily Lillie <emilylillie3@gmail.com>
2019-08-23 23:41:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,19 +7,19 @@ class App < Sinatra::Base
     "#{@user_name.reverse}"
   end
 
-  get '/square/:number' do 
+   get '/square/:number' do 
     @num = params[:number]
     "#{@num.to_i ** 2}"
   end
- 
-  get '/say/:number/:phrase' do 
-    "#{params[:phrase]}" * params[:number].to_i 
-  end 
-  
- get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+   get '/say/:number/:phrase' do
+  "#{params[:phrase]}" * params[:number].to_i
+  end
+
+   get '/say/:word1/:word2/:word3/:word4/:word5' do
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
-  
+
    get '/:operation/:number1/:number2' do
     @op = params[:operation]
     @num1 = params[:number1].to_i
@@ -34,6 +34,5 @@ class App < Sinatra::Base
   elsif @op == 'divide'
       "#{@num1/@num2}"
   end
-
 
 end
