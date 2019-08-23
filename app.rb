3c752c7a6f410e180615f576a61b1599098a9351
Emily Lillie <emilylillie3@gmail.com>
2019-08-23 23:13:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/reversename/:name' do 
-    '#{@username.reverse}'
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
+  end
 
 end
