0c834100ec07425a555daf450d53ab0f1e476166
Emily Lillie <emilylillie3@gmail.com>
2019-08-23 23:40:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,20 +20,20 @@ class App < Sinatra::Base
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
-  get '/:operation/:number1/:number2' do 
+   get '/:operation/:number1/:number2' do
     @op = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    
-    if @op == 'add'
+
+   if @op == 'add'
       "#{@num1 + @num2}"
-      elsif @op == 'subtract'
+  elsif @op == 'subtract'
       "#{@num1 - @num2}"
-      elsif @op == 'multiply'
+  elsif @op == 'multiply'
       "#{@num1 * @num2}"
-      elsif @op == 'divide'
+  elsif @op == 'divide'
       "#{@num1/@num2}"
-    end 
+  end
 
 
 end
